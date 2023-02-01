d1fdd48158513a351a6fb8d4c5bbe47372f44958
ilica <ilikayaroslav@gmail.com>
2023-02-01 14:58:25 +0100

    1

--- movie-quiz/src/components/Nav/Nav.jsx
+++ movie-quiz/src/components/Nav/Nav.jsx
@@ -1,18 +1,22 @@
-import { NavLink } from "react-router-dom";
-import "./Nav.css";
+import { NavLink } from 'react-router-dom';
+import './Nav.css';
 
 const Nav = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Movie Quiz</NavLink>
-        </li>
-        <li>
-          <NavLink to="help">Help</NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to='/'>
+                        <h5>Movie Quiz</h5>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='help'>
+                        <h5>Help</h5>
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 export default Nav;
