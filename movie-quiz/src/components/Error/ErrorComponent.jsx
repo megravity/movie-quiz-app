2822e4a2316af063652eb2cc5297eef7fe8fded2
Marvin Deutz <marvin.deutz@yahoo.com>
2023-02-06 13:50:19 +0100

    Final commit - this is version 0.1

--- movie-quiz/src/components/Error/ErrorComponent.jsx
+++ movie-quiz/src/components/Error/ErrorComponent.jsx
@@ -1,11 +1,11 @@
-import './Error.css';
+import "./Error.css";
 
-import '../Error/Error.css';
+import "../Error/Error.css";
 const ErrorComponent = () => {
-    return (
-        <div className='error'>
-            <h1>404 Page not found :/</h1>
-        </div>
-    );
+  return (
+    <div className="error">
+      <h1>404 Page not found :/</h1>
+    </div>
+  );
 };
 export default ErrorComponent;
